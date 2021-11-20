595cd0519271bbefc132be8e60793f7ae630ad97
Bipperty <ignacio.gibbs@gmail.com>
2021-11-20 16:47:09 -0300

    Validacion de Email unico y hasheo del Password

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,18 +1,29 @@
 const User = require("../Models/users");
+const bcryptjs = require("bcryptjs");
 
 exports.CreateUsers = async (req, res) => {
   console.log("Prueba de Post Usuarios");
   console.log("Body: ", req.body);
   try {
-    let user;
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email: email }); // Validamos que el usuario sea unico por el registro de su email
+
+    if (user) {
+      return res.status(400).json({ message: "El usuario ya esta registrado" });
+    }
 
     user = new User(req.body);
 
-    await user.save();
+    const salt = await bcryptjs.genSalt(10); // Salt para el hash
 
-    res.status(200).send({ message: "Usuario guardado con exito" });
+    user.password = await bcryptjs.hash(password, salt); // Hashea el password
+
+    await user.save(); // Guarda el nuevo usuario en la DB
+
+    res.status(200).json({ message: "Usuario guardado con exito" });
   } catch (e) {
     console.log("Error en la creacion de usuario", e);
-    res.status(400).send({ message: "Error en la creacion de usuario" });
+    res.status(400).json({ message: "Error en la creacion de usuario" });
   }
 };
