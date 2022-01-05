9204a0ab38f97e2df6a09ccf147fcaa95c801c93
gaston chatelet <gastonchatelet@gmail.com>
2022-01-05 19:06:06 -0300

    cambios chate, error al trabajar en rama master

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -9,7 +9,7 @@ const privateKey = fs.readFileSync("./keys/private.pem");
 const CreateUsers = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const {filename, path} = req.file;
+    const { filename, path } = req.file;
 
     let user = await User.findOne({ email: email }); // Validamos que el usuario sea unico por el registro de su email
 
@@ -19,18 +19,18 @@ const CreateUsers = async (req, res) => {
 
     User.init();
 
-    user = new User (req.body);
-    
+    user = new User(req.body);
+
     const salt = await bcryptjs.genSalt(10); // Salt para el hash
 
     user.password = await bcryptjs.hash(password, salt); // Hashea el password
-    
+
     //subir images y la ruta de la imagen para el getAll
-     user.images = filename;
-     user.path = path
+    user.images = filename;
+    user.path = path
     console.log(user)
     await user.save(); // Guarda el nuevo usuario en la DB
-      
+
     const jwtOptions = { algorithm: "RS256", expiresIn: "1h" };
 
     const payload = { username: username };
@@ -43,17 +43,19 @@ const CreateUsers = async (req, res) => {
       message: "Usuario guardado con exito",
     });
   } catch (e) {
-    res.status(400).json({ message: `Error en la creacion de usuario + ${e.message}`  });
+    res.status(400).json({ message: `Error en la creacion de usuario + ${e.message}` });
   }
 };
 
-const getAll = async (req, res) =>{ try{ users = await User.find();res.json(users);}catch(e){ res.status(404).json({ message: "falla del getAll"})} };
+const getAll = async (req, res) => { try { users = await User.find(); res.json(users); } catch (e) { res.status(404).json({ message: "falla del getAll" }) } };
 
-const Single = async (req, res) =>{
-  try {single = await db.Users.findOne({_id = req.params.id});
-   res.json(single)
-  } catch(e){ 
-    res.status(404).json({ message:"error al traer single"})}
-  };
+const Single = async (req, res) => {
+  try {
+    single = await db.Users.findOne({ _id = req.params.id });
+    res.json(single)
+  } catch (e) {
+    res.status(404).json({ message: "error al traer single" })
+  }
+};
 
-module.exports = {CreateUsers, getAll, Single} 
+module.exports = { CreateUsers, getAll, Single } 
