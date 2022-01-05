63c85d8dc698fbc313a2ba93f4737ac8815dbb14
Bipperty <ignacio.gibbs@gmail.com>
2022-01-05 19:12:15 -0300

    Last Update

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const DBConnection = require("./config/DB");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const { createRoles } = require("./config/initialSetup");
 const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT_SERVER || 4500;
 const server = process.env.URL_SERVER || "http://localhost:";
+const router = require("./routes/index");
 const app = express();
 
 createRoles();
@@ -15,6 +17,8 @@ createRoles();
 dotenv.config(); // Configuracion del .env
 
 app.use(morgan("dev")); // Middleware para el manejo y vision de solicitudes HTTP por consola
+
+app.use(helmet());
 
 app.use(express.json({ extend: true })); // Habilito el formato JSON
 
